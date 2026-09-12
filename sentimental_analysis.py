3ac75f5f328730b098b86c6410d16a49c4f86e56
hadi-rahimii <0hadi.rahimi@gmail.com>
2026-09-12 11:44:37 +0330

    NLI

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -1,37 +1,34 @@
 import dotenv
 import os
 from openai import OpenAI
-
+from transformers import pipeline
 dotenv.load_dotenv()
 
 
 class Analyzer:
     def __init__(self):
-        self.llm = OpenAI(
-            api_key=os.getenv('GROQ_API_KEY'),
-            base_url="https://api.groq.com/openai/v1"
-
-        )
+        self.classifier = pipeline(
+            "zero-shot-classification", model="typeform/distilbert-base-uncased-mnli")
         self.messages = [{'role': 'system',
-                          'content': '''Classify thr dentimental of the text into positive, negative, or neutral. Provide a brief explanation for your classification.'''}]
+                          'content': '''Classify thr dentimental of the text into dance, work, or study.'''},]
 
     def sentiment_analysis(self, text):
-
-        response = self.llm.chat.completions.create(
-            model="openai/gpt-oss-20b",
-            messages=[*self.messages, {'role': 'user', 'content': text}],
-            temperature=0.7,
-            max_tokens=100,
-        )
-        return response.choices[0].message.content
+        labels = ["dance", "study", "work"]
+        return self.classifier(text, candidate_labels=labels)
 
 
 def main():
     analyzer = Analyzer()
-    sentiment = analyzer.sentiment_analysis(
-        "I love this product! It's amazing and exceeded my expectations.")
-    print("Sentiment Analysis Result:")
-    print(sentiment)
+    while True:
+        text = input(
+            '''enter some text ( type 'q' for terminate) : ''').strip().lower()
+        if text == 'q':
+            print('good luck')
+            break
+
+        sentiment = analyzer.sentiment_analysis(text)
+        print(sentiment['labels'])
+        print(sentiment['scores'])
 
 
 if __name__ == "__main__":
