1eb4d1230b1a227f09ea017fed0231d3afcbce3e
hadi-rahimii <0hadi.rahimi@gmail.com>
2026-09-15 15:41:14 +0330

    using chatprompttemplate

--- prompt_template.py
+++ prompt_template.py
@@ -1,7 +1,7 @@
 import dotenv
 from langchain_groq import ChatGroq
 from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 dotenv.load_dotenv()
 
 
@@ -10,20 +10,28 @@ class Agent:
         self.llm = ChatGroq(model='openai/gpt-oss-20b', temperature=0.5)
 
     def chat(self):
-        # response = self.llm.invoke('hello')
-        # print(response.content)
         
-        template = '''
-        Dear {user_name},
-        Appologizing for the bad expreince you had with our service. We are committed to improving and ensuring that such issues do not occur in the future. Your feedback is invaluable to us, and we appreciate your patience and understanding.
-        Sincerely,
-        {brand_name}
+        #///////////////////////
+        # creating template for almost static prompt
         
-        '''
+        # template = '''
+        # Dear {user_name},
+        # Appologizing for the bad expreince you had with our service. We are committed to improving and ensuring that such issues do not occur in the future. Your feedback is invaluable to us, and we appreciate your patience and understanding.
+        # Sincerely,
+        # {brand_name}
         
-        prompt = PromptTemplate(template=template, input_variables=['user_name', 'brand_name'])
+        # '''
+        # prompt = PromptTemplate(template=template, input_variables=['user_name', 'brand_name'])
+        # print(prompt.format(user_name='Hadi', brand_name='Tech Solutions Inc.'))
         
-        print(prompt.format(user_name='Hadi', brand_name='Tech Solutions Inc.'))
+        #///////////////////////////////////////
+        # usin chain for runnable prompt with llm
+        
+        prompt = ChatPromptTemplate.from_messages([{'role':"system",'content':'you are a translator from {origin} to {to}'},{'role':'user','content':'{input}'}])
+        
+        chain = prompt | self.llm
+        response = chain.invoke({"origin":'English', "to":'Persian', "input":'hello how are?'})
+        print(response.content)
 
 
 def main():
